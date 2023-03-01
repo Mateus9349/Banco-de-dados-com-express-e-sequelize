c079323771fcacb2f5e424556fd8cefacf32f67a
Mateus9349 <103339423+Mateus9349@users.noreply.github.com>
2023-03-01 12:46:54 -0400

    salvando no repositorio

---
+++ api/routes/index.js
@@ -0,0 +1,17 @@
+//routes/index.js
+
+const bodyParser = require('body-parser')
+
+const pessoas = require('./pessoasRoute')
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
+
+module.exports = app => {
+ app.use(
+   bodyParser.json(),
+   bodyParser.urlencoded({extended: false}),
+   pessoas,
+   niveis,
+   turmas
+   )
+ }
